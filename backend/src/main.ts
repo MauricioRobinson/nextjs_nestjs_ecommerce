4fc67cbc97a2fbe006f5d64ed265119b1f0c3a6b
Mauricio Robinson <marsjr97@gmail.com>
2024-03-29 17:50:56 -0400

    config: sets up global configurations props, validators and docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Enables CORS
+  app.enableCors();
+  //Enables header security
+  app.use(helmet());
+
+  //Adds API Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  //Sets global prefix after hostname
+  app.setGlobalPrefix('api/v1');
+
+  //Sets up Global Validation Pipes or request protection
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  //Set up Swagger for documentation
+  const config = new DocumentBuilder()
+    .setTitle('Next Ecommerce')
+    .setDescription('The Next Ecommerce API documentation')
+    .setVersion('1.0')
+    .addTag('APIs')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
